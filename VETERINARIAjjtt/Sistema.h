d37d87b5b0b753702945945b35be30157c22166f
Juan Jose Toro <jjtt48@gmail.com>
2022-03-03 10:11:31 -0500

    ADD: esqueleto de clases de sistema que faltaba

--- VETERINARIAjjtt/Sistema.h
+++ VETERINARIAjjtt/Sistema.h
@@ -1,4 +1,21 @@
 #ifndef SISTEMA_H
 #define SISTEMA_H
 
+#include<iostream>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+class Propietario{
+private:
+    string nombre;
+    double id;
+    string email;
+    double telefono;
+
+public:
+    Sistema();
+    
+};
 #endif
